3e133f67bf02cb5baa1eba5d37967e2e320294d7
Nico1609 <ndayana0309@gmail.com>
2025-10-28 22:36:39 -0500

    Actualización del submódulo

--- src/model/Computer.java
+++ src/model/Computer.java
@@ -2,17 +2,35 @@ package model;
 
 public class Computer {
 
+    private String serial;
+    private int floor;
+    private int column;
+    private ArrayList<Incident> incidents;
+
     /*
      * ATENCION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
      * Agregue los atributos (relaciones) necesarios para satisfacer los
      * requerimientos.
-     * RF1
-     * RF2
-     * RF3
      */
 
-    public Computer() {
+    public Computer(String serial, int floor, int column) {
+        this.serial = serial;
+        this.floor = floor;
+        this.column = column;
+        this.incidents = new ArrayList<>();
 
+    }
+
+    public String getSerial(){
+        return serial;
+    }
+
+    public int getFloor(){
+        return floor;
+    }
+
+    public ArrayList<Incident> getIncidents(){
+        return incidents;
     }
 
     /*
@@ -21,8 +39,12 @@ public class Computer {
      * Agregue los parametros y retorno que sean pertinentes.
      * Agregue la logica necesaria.
      */
-    public void addIncident() {
+    public void addIncident(Incident i) {
+        incidents.add(i);
 
+    }
+    public int getIncidentCount(){
+        return incidents.size();
     }
 
 }
--- src/model/SchoolController.java
+++ src/model/SchoolController.java
@@ -10,6 +10,13 @@ public class SchoolController {
 
     public SchoolController() {
 
+        private Computer[][] computers;
+        private int floors = 5;
+        private int columns = 10;
+
+        public SchoolController(){
+            computers = new Computer [floors][columns];
+        }
     }
 
     /*
@@ -22,14 +29,54 @@ public class SchoolController {
      * Agregue la logica necesaria (instrucciones) para satisfacer los
      * requerimientos.
      */
-    public void agregarComputador() {
+
+    /*@param serial número serial único
+      *@param floor piso donde se ubicará (1 a 5)
+      *@return mensaje con el resultado de la operación
+      *Precondición: el piso debe estar entre 1 y 5
+      *Postcondición: el computador queda almacenado en la primera columna libre
+      */
+    public String agregarComputador(String serial, int floor) {
+        if (findComputerBySerial(serial) != null){
+            return "Error: ya existe un computador con ese serial.";
+        }
+
+        if (floor <1 || floor > floors){
+            return "Error: piso inválido";
+        }
+
+        for (int c= 0; c < columns; c++){
+            if (computers[floor -1][c] == null){
+                computers[floor - 1][c] = new Computer(serial, floor, c + 1);
+                return " Computador agregado correctamente en el piso " + floor + ", columna " + (c + 1);
+            }
+        }
+
+        return " No hay espacio disponible en ese piso";
 
     }
 
-    public void agregarIncidenteEnComputador() {
+    /*
+     * Registra un incidente en el computador correspondiente
+     * @param serial número serial del computador
+     * @param description descripción del incidente
+     * @return mensaje de confirmación o error
+     */
+    public String agregarIncidenteEnComputador(String serial, String description) {
+        Computer comp = findComputerBySerial (serial);
+        if (comp != null){
+            comp.addIncident(new Incident(description));
+            return "Incidente registrado al computador" + serial;
+        } else{
+            return " No se encontró el computador con ese serial ";
+        }
 
     }
 
+    /*
+     *Busca y devuelve el computador con más incidentes
+    +@return el computador con más incidentes o null si no hay
+     */
     public void getComputerList() {
 
     }
